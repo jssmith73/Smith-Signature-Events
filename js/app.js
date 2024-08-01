4e1c87fb9f819ff065ea48309c7cae8892278bfb
jman1359 <josiahsmith1359@gmail.com>
2024-08-01 07:27:30 -0400

    Angular.js testing

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("myApp", []);
+
+app.controller("header-control", function($scope) {
+$scope.firstname = "Josiah";
+$scope.lastname = "Smith";
+console.log('Header parsed')
+});
+
+
+//Test JS Script
+
+// var tester = document.getElementById("test");
+// tester.textContent = "Test Complete";
+
